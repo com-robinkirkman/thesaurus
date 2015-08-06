278fea02f1b25f8ef8c22ae72f8bf10b7f55247a
robin <robin.kirkman@gmail.com>
2015-08-05 22:47:02 -0400

    thesaurus initial commit

---
+++ thesaurus/src/main/java/com/robinkirkman/thesaurus/Thesaurus.java
@@ -0,0 +1,42 @@
+package com.robinkirkman.thesaurus;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class Thesaurus {
+
+	private static Thesaurus instance;
+	
+	public static synchronized Thesaurus getInstance() {
+		if(instance == null)
+			instance = new Thesaurus();
+		return instance;
+	}
+
+	private Map<String, List<String>> map;
+	
+	private Thesaurus() {
+		try {
+			BufferedReader buf = new BufferedReader(new InputStreamReader(Thesaurus.class.getResourceAsStream("mthesaur.txt"), "UTF-8"));
+			for(String line = buf.readLine(); line != null; line = buf.readLine()) {
+				List<String> group = Collections.unmodifiableList(Arrays.asList(line.split(",")));
+				for(String word : group) {
+					map.put(word, group);
+				}
+			}
+		} catch(IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public List<String> get(String word) {
+		return map.get(word);
+	}
+}
